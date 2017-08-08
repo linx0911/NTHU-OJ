18bcced4122bd31959246523a0348bdf5c9e1545
linx0911 <linxgray0911@gmail.com>
2017-08-09 02:58:55 +0800

    Update Adding_Decimals.cpp

--- Adding_Decimals.cpp
+++ Adding_Decimals.cpp
@@ -1,25 +1,20 @@
 #include<cstdio>
-#include<cmath>
 #include<cstring>
 
 int main(){
     char A[1005], B[1005], C[1005];
-    while(scanf("%s %s", A, B)!=EOF)
-    {
-        int A_index,B_index, C_index = 1004,ans = 0, carry = 0;
-        A_index = strlen(A) - 1;    //O(n)
-        B_index = strlen(B) - 1;    //O(n)
+    while(scanf("%s %s", A, B)!=EOF){
+        int A_index = strlen(A) - 1,B_index = strlen(B) - 1, C_index = 1004;
+        int ans = 0, carry = 0, a, b, c;
+
         C[C_index--] = '\0';
         while( (A_index >= 0 || B_index >= 0) || carry > 0){
-            int a = (A_index >= 0) ? (A[A_index] - '0') : 0;
-            int b = (B_index >= 0) ? (B[B_index] - '0') : 0;
-            int c = a + b + carry;
+            a = (A_index >= 0) ? (A[A_index] - '0') : 0;
+            b = (B_index >= 0) ? (B[B_index] - '0') : 0;
+            c = a + b + carry;
             carry = c / 10;
-            char tmp = c % 10 + '0';
-            C[C_index] = tmp;
-            A_index--;
-            B_index--;
-            C_index--;
+            C[C_index] = c % 10 + '0';
+            A_index--;  B_index--;  C_index--;
             if(carry > 0) ans++;
         }
         for(int i = C_index+1; i < 1004; i++) printf("%c", C[i]);
