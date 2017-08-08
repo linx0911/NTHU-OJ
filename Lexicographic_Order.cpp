db46cb2d32448cec4fb10a19209e045bb4225053
linx0911 <linxgray0911@gmail.com>
2017-08-09 02:46:56 +0800

    Update Lexicographic_Order.cpp

--- Lexicographic_Order.cpp
+++ Lexicographic_Order.cpp
@@ -1,22 +1,19 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
-
 using namespace std;
-struct str{
-    char name[51];
-};
+
 int compare(const void *a,const void *b){
-    return strcmp(((str*)a)->name, ((str*)b)->name);
+    return strcmp((char*)a, (char*)b);
 }
 int main(){
     int n;
     while(scanf("%d",&n) != EOF){
         if(n == 0) break;
-        str people[n];
-        for(int i = 0; i < n; i++) scanf("%s",people[i].name);
-        qsort(people, n, sizeof(str), compare);
-        for(int i = 0; i < n; i++) printf("%s\n",people[i].name);
+        char input[n][52];
+        for(int i = 0; i < n; i++) scanf("%s",input[i]);
+        qsort(input, n, sizeof(input[0]), compare);
+        for(int i = 0; i < n; i++) printf("%s\n",input[i]);
         printf("\n");
     }
 	return 0;
